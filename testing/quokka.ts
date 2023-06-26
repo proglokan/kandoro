8ba61e8a7c38610766c853365c0e0dff51e46bc4
proglokan <92769441+proglokan@users.noreply.github.com>
2023-06-26 09:40:59 -0700

    Pomodoro 1.0.0

--- testing/quokka.ts
+++ testing/quokka.ts
@@ -1,15 +1,29 @@
 'use strict';
-class Test {
-	private _num: number;
-	private _str: string;
+class Animal {
+	readonly name: string;
+	readonly toy: string;
 
-	constructor(num: number, str: string) {
-		this._num = num;
-		this._str = str;
+	constructor(name: string, toy: string) {
+		this.name = name;
+		this.toy = toy;
 	}
 
-	logParams() {
-		console.log(this._num, this._str);
+	speak(): void {
+		console.log(`${this.name} makes a sound.`);
 	}
-
 }
+
+class Dog extends Animal {
+	private breed: string;
+	constructor(name: string, toy: string, breed: string) {
+		super(name, toy);
+		this.breed = breed;
+	}
+
+	fetch(): void {
+		console.log(`${this.name}, the ${this.breed} fetches a ${this.toy}.`);
+	}
+}
+const dog: Dog = new Dog('Buddy', 'ball', 'Labrador');
+dog.speak();
+dog.fetch(); 
